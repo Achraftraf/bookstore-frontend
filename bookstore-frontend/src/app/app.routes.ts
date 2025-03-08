78e8ef048575aa76815e64dee720b7347d4e9156
Achraftraf <achrafzarouki20@gmail.com>
2025-03-08 23:49:22 +0000

    i create components also i handle the routes in the application

--- bookstore-frontend/src/app/app.routes.ts
+++ bookstore-frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },  // Default redirect
+  { path: 'books', component: BookListComponent },       // List books
+  { path: 'books/add', component: BookFormComponent },   // Add a book
+  { path: 'books/edit/:id', component: BookFormComponent } // Edit book by ID
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
